a4fc0befd1dc56e749cdd98e561a0b94405c883d
pavel <bern86@gmail.com>
2012-05-09 00:46:17 +0400

    fixed

--- src/main/scala/twitter/crawler/metrics/SubgraphBuilder.scala
+++ src/main/scala/twitter/crawler/metrics/SubgraphBuilder.scala
@@ -19,9 +19,6 @@ class SubgraphBuilder(names: Seq[String]) {
         val timestamps = SortedSet(getUserUrlsTs(name, 0l, currentTs)._2: _*)
         if (timestamps.size >= 10) {
           tsMap(name) = timestamps
-          val distr = new SingleDistribution(timestamps.head, timestamps.last + 1, timestamps)
-          conditionalEntropy(name) = computeCondEntropy(distr.computeCounters(INTERVALS), distr.total)
-          println("save "+name+" "+conditionalEntropy(name))
         }
     }
   }
@@ -35,12 +32,15 @@ class SubgraphBuilder(names: Seq[String]) {
           k2 =>
             val ts2 = tsMap(k2)
             val (left, right) = borders(ts1, ts2)
-            val distr = new SingleDistribution(left, right, ts1)
+
+            val distr = new SingleDistribution(left, right, ts1.range(left, right+1))
             val cE1 = computeCondEntropy(distr.computeCounters(INTERVALS), distr.total)
+
             val jd = new JoinedDistribution(left , right, ts2.range(left, right+1), ts1.range(left, right+1))
             val entr = computeCondEntropy(jd.computeCounters(INTERVALS), jd.total)
             println("joined entropy "+k2+" to"+k1+" "+entr)
             val it = cE1 - entr
+
             println("save edge from "+k2+" to "+k1+" "+it)
             ITFile.write("%s, %s, %s\n".format(k2, k1, it))
         }
